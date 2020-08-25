7525db0502b5616415aa062a9265a87548515403
Andrewss <Andrewss@BI2043.babraham.ac.uk>
2020-08-25 12:21:18 +0100

    Fix typo in R script

--- R/create_sleuth_graph.R
+++ R/create_sleuth_graph.R
@@ -12,7 +12,7 @@ str_replace(input_file,".txt$",".png") -> output_file
 # Read the input file
 read_tsv(input_file) -> sleuth_data
 
-# Get the total obervation count for each SNP
+# Get the total observation count for each SNP
 sleuth_data %>%
   mutate(TOTAL=REFCOUNT+ALTCOUNT+OTHERCOUNT) -> sleuth_data
 
